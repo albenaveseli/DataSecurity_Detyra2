7b09ffcab6fc5e4eff9ffaeec10a0b386893bd8c
agnesasuhodolli <agnesa.suhodolli@student.uni-pr.edu>
2025-04-06 17:08:58 +0200

    Update MorseCodee.GeneratedMSBuildEditorConfig.editorconfig

--- MorseCodee/MorseCode/MorseMap.cs
+++ MorseCodee/MorseCode/MorseMap.cs
@@ -1,6 +1,29 @@
-﻿namespace MorseCodee.MorseCode;
+﻿using System.Collections.Generic;
 
-public class MorseMap
+namespace MorseCodeApp.MorseCode
 {
-    
+    public static class MorseMap
+    {
+        public static readonly Dictionary<char, string> TextToMorse = new()
+        {
+            {'A', ".-"},    {'B', "-..."},  {'C', "-.-."}, {'D', "-.."},   {'E', "."},
+            {'F', "..-."},  {'G', "--."},   {'H', "...."}, {'I', ".."},    {'J', ".---"},
+            {'K', "-.-"},   {'L', ".-.."},  {'M', "--"},   {'N', "-."},    {'O', "---"},
+            {'P', ".--."},  {'Q', "--.-"},  {'R', ".-."},  {'S', "..."},   {'T', "-"},
+            {'U', "..-"},   {'V', "...-"},  {'W', ".--"},  {'X', "-..-"},  {'Y', "-.--"},
+            {'Z', "--.."},  {'0', "-----"}, {'1', ".----"},{'2', "..---"}, {'3', "...--"},
+            {'4', "....-"}, {'5', "....."}, {'6', "-...."},{'7', "--..."}, {'8', "---.."},
+            {'9', "----."}, {' ', "/"}
+        };
+
+        public static readonly Dictionary<string, char> MorseToText = new();
+
+        static MorseMap()
+        {
+            foreach (var kvp in TextToMorse)
+            {
+                MorseToText[kvp.Value] = kvp.Key;
+            }
+        }
+    }
 }
