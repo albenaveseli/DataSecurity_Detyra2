a86550aa9b86f2378973febea5642477aa89b3e0
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-04-06 21:46:21 +0200

    MorseEncoder-i

--- MorseCodee/MorseCode/MorseEncoder.cs
+++ MorseCodee/MorseCode/MorseEncoder.cs
@@ -1,6 +1,21 @@
-﻿namespace MorseCodee.MorseCode;
+﻿
+using System.Text;
 
-public class MorseEncoder
+namespace MorseCodeApp.MorseCode
 {
-    
+    public class MorseEncoder
+    {
+        public string Encode(string text)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in text.ToUpper())
+            {
+                if (MorseMap.TextToMorse.TryGetValue(c, out var morse))
+                {
+                    sb.Append(morse).Append(" ");
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
 }
