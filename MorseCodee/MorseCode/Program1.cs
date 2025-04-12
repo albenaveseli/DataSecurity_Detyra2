f0e6e8db51634100cb498d0a70fe49d516af1f98
AnitaAhmeti1 <anita.ahmeti2@student.uni-pr.edu>
2025-04-12 17:16:34 +0200

    Updated Program1

---
+++ MorseCodee/MorseCode/Program1.cs
@@ -0,0 +1,21 @@
+using System;
+using MorseCodee.MorseCode;
+
+namespace MorseCodee
+{
+    class Program
+    {
+        static void Main()
+        {
+        //pjesa1
+
+        //pjesa2
+
+
+        //pjesa3
+
+
+        //pjesa 4
+        }
+    }
+}
